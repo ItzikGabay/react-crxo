27fd95970907ec800c3060fa229653344e88ca79
Itzikgabay <itzikdevio@gmail.com>
2022-10-02 23:27:46 +0300

    Bugfix forgot to send value to addTemplateHooks

--- src/react-library/template.js
+++ src/react-library/template.js
@@ -3,7 +3,7 @@ import { addTemplateImports, addTemplateHooks } from './imports.js';
 export const JSXtemplate = (name, uppercased, cssType, templateType) => {
   return `${addTemplateImports(templateType, uppercased, cssType)}
 const ${uppercased} = () => {
-  ${addTemplateHooks()}
+  ${addTemplateHooks(templateType)}
   return (
     <div className={styles.container}>
       <p className={styles.title}>Component created successfully.</p>
