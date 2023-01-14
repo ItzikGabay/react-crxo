6a49b9be1626407a5d841e8240cf07ebfcfde390
Itzikgabay <itzikdevio@gmail.com>
2023-01-14 13:16:36 +0200

    Add more errors handling

--- src/lib/language.js
+++ src/lib/language.js
@@ -6,6 +6,9 @@ const language = {
     'Invalid usage. you cannot use special characters in the name or the path.',
   ERROR_RENDERING_ERR: "Couldn't be rendered in the current environment.",
   SOMETHING_WRONG_ERR: 'Something went wrong. please try again.',
+  NO_ARGUMENTS_ERR: 'No arguments provided.',
+  NO_CMP_NAME_ERR: 'No component name found. Please provide a component name.',
+  PROVIDE_VALID_NAME_ERR: 'Please provide a valid component name.',
 };
 
 export default language;
