defb0ec82425d18ff31d17606a57d4440aacaca8
Itzikgabay <itzikdevio@gmail.com>
2023-02-15 22:06:32 +0200

    chore: Add `questions` export

--- src/lib/language.js
+++ src/lib/language.js
@@ -1,4 +1,4 @@
-const language = {
+const language = Object.freeze({
   INVALID_CHARACTER_NUM_ERR: 'Invalid usage. you cannot use empty value.',
   INVALID_USAGE_ERR:
     'Invalid usage. please use crxo -c [path] [name] [options]',
@@ -13,6 +13,20 @@ const language = {
     'Directory already exists. Please provide a new name.',
   INTERNAL_ERR: 'Internal error. Please try again: ',
   THANK_YOU_MSG: 'Thank you for using crxo!',
-};
+});
+
+export const questions = Object.freeze({
+  QUESTION_COMPONENT_NAME: 'Whats is the component name?',
+  QUESTION_FILE_TYPES: 'Which files do you want to generate?',
+  QUESTION_TEMPLATE: 'Which template do you want to use?',
+  QUESTION_TEMPLATE_LITE: '(lite) - no state, no props',
+  QUESTION_TEMPLATE_REGULAR: '(basic) - useState, useEffect',
+  QUESTION_TEMPLATE_LARGE: '(extended) basic + getServerSideProps',
+  QUESTION_CONVENTION: 'Which convention do you want to use?',
+  QUESTION_CONVENTION_DEFAULT: 'ComponentName (default)',
+  QUESTION_CONVENTION_CAMEL: 'componentName',
+  QUESTION_CONVENTION_KEBAB: 'component-name',
+  QUESTION_CONVENTION_SNAKE: 'component_name',
+});
 
 export default language;
